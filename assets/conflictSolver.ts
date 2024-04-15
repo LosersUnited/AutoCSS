b8ba9b1423095f20d71fc2390d2086b3975b8356
Daniel <davilareko@gmail.com>
2024-04-15 02:37:21 +0200

    Make conflict solver prefer less props when possible

--- assets/conflictSolver.ts
+++ assets/conflictSolver.ts
@@ -20,16 +20,42 @@ const stages: { [key: number]: (targetModule: { [key: string]: any }, targetProp
         const proxy = await fetcher.fetchFullDiscordCSSDefinitions();
         const targetModuleKeys = Object.keys(targetModule);
         const targetAmountOfPropertiesToPick = ((targetModuleKeys.length % 2 == 0) ? targetModuleKeys.length : targetModuleKeys.length - 1);
-        for (let index = 0; index < 3; index++) {
+        for (let index = 0; index < 6; index++) {
             const randomProps = utils.pickRandomProperties(targetModule, targetAmountOfPropertiesToPick > 1 ? (targetAmountOfPropertiesToPick / 2) : targetAmountOfPropertiesToPick);
-            const targetProps = [...randomProps, targetProp];
+            const targetProps = randomProps.includes(targetProp) ? randomProps : [...randomProps, targetProp];
             const constructed = {
                 isUnique: proxy.filter(x =>
                     targetProps.every(key => x && x.hasOwnProperty(key))).length == 1,
                 recipe: targetProps
             };
             if (constructed.isUnique)
+            // return constructed;
+            {
+                if (constructed.recipe.length > 4) {
+                    let currentMaxPropCount = constructed.recipe.length - 2;
+                    let last = { isUnique: false, recipe: [] } as SolveResult;
+                    // for (let index2 = 0; index2 < 32; index2++) {
+                    for (let index2 = 0; index2 < targetModuleKeys.length; index2++) {
+                        const randomProps2 = utils.pickRandomProperties(targetModule, currentMaxPropCount);
+                        const targetProps2 = randomProps2.includes(targetProp) ? randomProps2 : [...randomProps2, targetProp];
+                        // if (randomProps2.length == 0)
+                        //     continue;
+                        const constructed2 = {
+                            isUnique: proxy.filter(x =>
+                                targetProps2.every(key => x && x.hasOwnProperty(key))).length == 1,
+                            recipe: targetProps2
+                        };
+                        currentMaxPropCount--;
+                        if (constructed2.isUnique) {
+                            last = constructed2;
+                        }
+                        else {
+                            return last;
+                        }
+                    }
+                }
                 return constructed;
+            }
         }
         return {
             isUnique: false,
